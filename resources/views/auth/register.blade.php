6b97462787e16944b8869d66743f2de260ea514d
marsked <marsked@163.com>
2018-06-05 11:24:12 +0800

    login regist

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,189 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+	<style type="text/css">
+		body{
+			background-color:#151515;
+		}
+		.form-horizontal{
+		    background: #fff;
+		    padding-bottom: 40px;
+		    border-radius: 15px;
+		    text-align: center;
+		}
+		.form-horizontal .heading{
+		    display: block;
+		    font-size: 35px;
+		    font-weight: 700;
+		    padding: 35px 0;
+		    border-bottom: 1px solid #f0f0f0;
+		    margin-bottom: 30px;
+		}
+		.form-horizontal .form-group{
+		    padding: 0 40px;
+		    margin: 0 0 25px 0;
+		    position: relative;
+		}
+		.form-horizontal .form-control{
+		    background: #f0f0f0;
+		    border: none;
+		    border-radius: 20px;
+		    box-shadow: none;
+		    padding: 0 20px 0 45px;
+		    height: 40px;
+		    transition: all 0.3s ease 0s;
+		}
+		.form-horizontal .form-control:focus{
+		    background: #e0e0e0;
+		    box-shadow: none;
+		    outline: 0 none;
+		}
+		.form-horizontal .form-group i{
+		    position: absolute;
+		    top: 12px;
+		    left: 60px;
+		    font-size: 17px;
+		    color: #c8c8c8;
+		    transition : all 0.5s ease 0s;
+		}
+		.form-horizontal .form-control:focus + i{
+		    color: #FFAA00;
+		}
+		.form-horizontal .fa-question-circle{
+		    display: inline-block;
+		    position: absolute;
+		    top: 12px;
+		    right: 60px;
+		    font-size: 20px;
+		    color: #808080;
+		    transition: all 0.5s ease 0s;
+		}
+		.form-horizontal .fa-question-circle:hover{
+		    color: #000;
+		}
+		.form-horizontal .main-checkbox{
+		    float: left;
+		    width: 20px;
+		    height: 20px;
+		    background: #FFAA00;
+		    border-radius: 50%;
+		    position: relative;
+		    margin: 5px 0 0 5px;
+		    border: 1px solid #FFAA00;
+		}
+		.form-horizontal .main-checkbox label{
+		    width: 20px;
+		    height: 20px;
+		    position: absolute;
+		    top: 0;
+		    left: 0;
+		    cursor: pointer;
+		}
+		.form-horizontal .main-checkbox label:after{
+		    content: "";
+		    width: 10px;
+		    height: 5px;
+		    position: absolute;
+		    top: 5px;
+		    left: 4px;
+		    border: 3px solid #fff;
+		    border-top: none;
+		    border-right: none;
+		    background: transparent;
+		    opacity: 0;
+		    -webkit-transform: rotate(-45deg);
+		    transform: rotate(-45deg);
+		}
+		.form-horizontal .main-checkbox input[type=checkbox]{
+		    visibility: hidden;
+		}
+		.form-horizontal .main-checkbox input[type=checkbox]:checked + label:after{
+		    opacity: 1;
+		}
+		.form-horizontal .text{
+		    float: left;
+		    margin-left: 7px;
+		    line-height: 20px;
+		    padding-top: 5px;
+		    text-transform: capitalize;
+		}
+		.form-horizontal .btn{
+            margin-right: auto;
+            margin-left: auto;
+		    font-size: 14px;
+		    color: #fff;
+		    background: #FFAA00;
+		    border-radius: 30px;
+		    padding: 10px 25px;
+		    border: none;
+		    text-transform: capitalize;
+		    transition: all 0.5s ease 0s;
+		}
+		@media only screen and (max-width: 479px){
+		    .form-horizontal .form-group{
+		        padding: 0 25px;
+		    }
+		    .form-horizontal .form-group i{
+		        left: 45px;
+		    }
+		    .form-horizontal .btn{
+		        padding: 10px 20px;
+		    }
+		}
+	</style>
+</head>
+<body>
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+    <div class="container">
+        <div class="row">
+        	<div class="col-md-offset-3 col-md-6" style='text-align: center;margin-top:100px;'>
+            	<a href="{{ url('/') }}"><img src="/homes/images/icons/logo-orange.png"></a>
+        	</div>
+            <div class="col-md-offset-3 col-md-6" style="margin-top:20px;">
+                <form  class="form-horizontal" method="POST" action="{{ route('register') }}">
+                    {{ csrf_field() }}
+                    <span class="heading">Create An Account</span>
+                    <div class="form-group">
+                        <input id="inputEmail3" placeholder="Name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus >
+                        @if ($errors->has('name'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('name') }}</strong>
+                            </span>
+                        @endif
+                        <i class="glyphicon glyphicon-user"></i>
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control" id="inputEmail3" placeholder="Email" name="email" value="{{ old('email') }}" required autofocus>
+                        @if ($errors->has('email'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
+                        <i class="glyphicon glyphicon-comment"></i>
+                    </div>
+                    <div class="form-group help">
+                        <input type="password" class="form-control" id="inputPassword3" placeholder="PassWord" name="password" required>
+                        @if ($errors->has('password'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                        @endif
+                        <i class="glyphicon glyphicon-lock"></i>
+                    </div>
+                    <div class="form-group help">
+                        <input type="password" class="form-control" id="inputPassword3" placeholder="Confirm PassWord" name="password_confirmation" required>
+                        <i class="glyphicon glyphicon-lock"></i>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-default">确认注册</button>
+                        <a href="{{ route('login') }}" class="btn btn-default">去登录</a>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-</div>
-@endsection
+
+</body>
+</html>
